9775c4cb4c6d88fe80b1165dc9cdbfb709d3fe65
AbdulDunggio <dunggioalil53@gmail.com>
2025-01-23 10:17:10 +0700

    Update delivery_performance_analysis.sql

--- delivery_performance_analysis.sql
+++ delivery_performance_analysis.sql
@@ -17,3 +17,26 @@ on c.customer_id = o.customer_id)
 select
 on_time * 100 / all_data as delivered_percentage
 from on_time_cte, all_data_cte
+
+--calculate the city that have the highest level of delay --
+
+select
+customer_city,
+count(*) as late_count
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+where order_delivered_timestamp >= order_estimated_delivery_date
+group by customer_city
+order by late_count desc
+
+-- calculate average delivery duration per product category --
+select product_category_name,
+avg(datediff(order_delivered_timestamp, order_approved_at)) as rataan_product
+from products p
+join order_items oi
+on p.product_id = oi.product_id
+join orders o
+on o.order_id = oi.order_id
+group by product_category_name
+order by rataan_product desc
