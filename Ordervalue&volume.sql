a643a0acb5f8d020dcb8931f86036858dc35d34e
AbdulDunggio <dunggioalil53@gmail.com>
2025-01-24 00:24:48 +0700

    Update Ordervalue&volume.sql

--- Ordervalue&volume.sql
+++ Ordervalue&volume.sql
@@ -22,3 +22,39 @@ on p.order_id = o.order_id
 group by customer_id
 order by c_value desc
 limit 1
+
+-- analyze the relationship between purchase value and payment type --
+WITH sort_value AS (
+    SELECT
+        payment_value,
+        order_id, -- Tambahkan order_id di sini
+        ROW_NUMBER() OVER (ORDER BY payment_value) AS row_num,
+        COUNT(*) OVER () AS total_rows
+    FROM payments
+),
+value_ctg AS (
+    SELECT
+        MAX(CASE WHEN row_num <= total_rows * 0.33 THEN payment_value END) AS v_low,
+        MAX(CASE WHEN row_num > total_rows * 0.33 AND row_num <= total_rows * 0.66 THEN payment_value END) AS v_mid
+    FROM sort_value
+),
+value_class AS (
+    SELECT
+        sv.payment_value,
+        sv.order_id, -- Pastikan order_id diteruskan
+        CASE 
+            WHEN sv.payment_value <= vc.v_low THEN 'Low'
+            WHEN sv.payment_value <= vc.v_mid THEN 'Normal'
+            ELSE 'High'
+        END AS v_class
+    FROM sort_value sv
+    CROSS JOIN value_ctg vc
+)
+SELECT
+    vcl.v_class,
+    payment_type,
+    COUNT(p.payment_type) AS total_pay
+FROM value_class vcl
+JOIN payments p ON p.order_id = vcl.order_id
+GROUP BY vcl.v_class, payment_type
+ORDER BY total_pay DESC;
